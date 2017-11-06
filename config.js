b7e1898bdde60eb09cc329cbad10ceea0abf08ef
niftylettuce <niftylettuce@gmail.com>
2017-11-05 22:07:32 -0500

    More docs, CNAME setup, Fixed lodash deps

---
+++ config.js
@@ -0,0 +1,15 @@
+docute.init({
+  debug: true,
+  title: 'ForwardEmail',
+  repo: 'niftylettuce/forward-email',
+  'edit-link': 'https://github.com/niftylettuce/forward-email/tree/master/',
+  twitter: 'niftylettuce',
+  nav: {
+    default: [
+      {
+        title: 'Free, Secure, and Private Email Forwarding Service',
+        path: '/'
+      }
+    ]
+  }
+});
